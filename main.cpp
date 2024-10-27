8e3126ddfc47216e7ad463b9ee63d2ce39bfd724
emmett <emmettzhang2020@outlook.com>
2024-10-27 12:54:13 +0000

    add main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
-#include <print>
+int Print() {
+  int n = 1;
+  if (n == 1) {
+    ++n;
+  } else {
+    --n;
+  }
+  return n;
+}
 
 int main() {
-  std::print("hello world\n");
+  Print();
   return 0;
 }
