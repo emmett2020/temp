fbc5fa6fd821de80bedfe5a4f0ac2ee34e5c03f8
emmett <emmettzhang2020@outlook.com>
2024-10-29 13:33:31 +0000

    add

--- tools/compiler/install_gcc_use_ppa_on_ubuntu2204.sh
+++ tools/compiler/install_gcc_use_ppa_on_ubuntu2204.sh
@@ -1,4 +1,4 @@
 set -ex
-sudo apt install software-properties-common
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo apt install software-properties-common -y
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 sudo apt update
