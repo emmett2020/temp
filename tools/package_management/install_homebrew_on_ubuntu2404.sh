daee8705366ccc0f98e36af9fd6d5f3a06c5ac94
emmett <emmettzhang2020@outlook.com>
2024-10-29 14:49:25 +0000

    update

--- tools/package_management/install_homebrew_on_ubuntu2404.sh
+++
@@ -1,4 +0,0 @@
-# https://brew.sh/
-NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-
---
+++ tools/package_manager/install_homebrew_on_ubuntu2404.sh
@@ -0,0 +1,6 @@
+# https://brew.sh/
+NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Set environemnt variable
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /root/.bashrc
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
